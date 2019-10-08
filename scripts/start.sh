3d4f8f2fe9097c4f3d99e54b0c18c115b79995b9
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-10-08 23:53:43 +0300

    Docker Wrapper Script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
-# f5-terraform - Docker Wrapper Script
-# https://github.com/ArtiomL/f5-terraform
+# aws-terraform - Docker Wrapper Script
+# https://github.com/ArtiomL/aws-terraform
 # Artiom Lichtenstein
-# v1.0.5, 20/10/2018
+# v1.0.6, 08/10/2019
 
 # Extensibility
 if [[ ! -z "$REPO" ]]; then
 	git clone "https://github.com/$REPO.git"
 	cd $(echo "$REPO" | cut -d"/" -f2)
-fi
-
-# Azure RM environment variables
-if [[ -f "/home/user/.azure/credentials" ]]; then
-	export $(cat /home/user/.azure/credentials | tr '\n' ' ')
-	export $(cat /home/user/.azure/credentials | tr '[:upper:]' '[:lower:]' | sed 's/^/TF_VAR_/' | tr '\n' ' ')
-fi
-
-# GCP environment variables
-if [[ -f "/home/user/.gcp/credentials" ]]; then
-	export GOOGLE_APPLICATION_CREDENTIALS=/home/user/.gcp/credentials
-	export GOOGLE_PROJECT=$(jq -r '.project_id' /home/user/.gcp/credentials)
-fi
-
-# DigitalOcean environment variables
-if [[ -f "/home/user/.do/credentials" ]]; then
-	export $(cat /home/user/.do/credentials | tr '\n' ' ')
-	export $(cat /home/user/.do/credentials | tr '[:upper:]' '[:lower:]' | sed 's/^/TF_VAR_/' | tr '\n' ' ')
 fi
 
 exec /bin/sh
